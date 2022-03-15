b2e3d45848b49c133a1c0a5957b29410214d90c0
kevabra <97968059+kevabra@users.noreply.github.com>
2022-03-15 13:41:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,12 +41,15 @@ class Weather extends React.Component {
       data: {},
       input: "",
       weatherResults: [],
-      longLatInput: ""
+      longLatInput: "",
+      zipCodeInput: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.doCity = this.doCity.bind(this);
     this.handleChange2 = this.handleChange2.bind(this);
     this.doLongLat = this.doLongLat.bind(this);
+    this.handleChange3 = this.handleChange3.bind(this);
+    this.doZip = this.doZip.bind(this);
   }
   
   async componentDidMount() {
@@ -66,7 +69,8 @@ class Weather extends React.Component {
         data: api_result,
         input: this.state.input,
         weatherResults: this.state.weatherResults,
-        longLatInput: this.state.longLatInput
+        longLatInput: this.state.longLatInput,
+        zipCodeInput: this.state.zipCodeInput
       });
         
         
@@ -88,7 +92,8 @@ class Weather extends React.Component {
       data: this.state.data, 
       input: event.target.value,
       weatherResults: this.state.weatherResults,
-      longLatInput: this.state.longLatInput
+      longLatInput: this.state.longLatInput,
+      zipCodeInput: this.state.zipCodeInput
     });
   }
 
@@ -99,8 +104,21 @@ class Weather extends React.Component {
       data: this.state.data,
       input: this.state.input,
       weatherResults: this.state.weatherResults,
-      longLatInput: event.target.value
+      longLatInput: event.target.value,
+      zipCodeInput: this.state.zipCodeInput
     });
+  }
+
+  handleChange3(event) {
+    this.setState({
+      latitude: this.state.latitude,
+      longitude: this.state.longitude,
+      data: this.state.data,
+      input: this.state.input,
+      weatherResults: this.state.weatherResults,
+      longLatInput: this.state.longLatInput,
+      zipCodeInput: event.target.value
+    })
   }
 
   async doLongLat(event) {
@@ -202,6 +220,92 @@ class Weather extends React.Component {
     });
 
   }
+  //api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+  //https://api.openweathermap.org/data/2.5/weather?lat=39.6417629&lon=-77.7199932&appid=71e775c79529fd50aaefa8d695d32ff3
+
+  async doZip(event) {
+    let values = document.getElementById("zipCode").value.split(",");
+    if (values.length != 2 || values[1].trim() == '') {
+      alert("Incorrect format: Enter zip code plus a comma followed by country name, such as: 10001, usa or 85001, usa");
+      this.setState({
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        data: this.state.data,
+        input: this.state.input,
+        weatherResults: this.state.weatherResults,
+        longLatInput: this.state.longLatInput,
+        zipCodeInput: ""
+      });
+      return;
+    }
+    let zipCode = values[0];
+    let country = values[1];
+    console.log(zipCode);
+    console.log(country);
+    if (country[0] == " ") {
+      country.split("").splice(0, 1);
+    }
+    country = country.trim();
+    console.log(country);
+    //zip={zip code},{country code}&appid={API key}
+    const api_link = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "," + country + "&appid=71e775c79529fd50aaefa8d695d32ff3";
+
+    let api_call = await fetch(api_link);
+    let api_result = await api_call.json();
+    console.log(api_result);
+    if (!api_result.name) {
+      alert("Location not found: Enter zip code plus a comma followed by country name, such as: 10001, us or 85001, us");
+      this.setState({
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        data: api_result,
+        input: this.state.input,
+        weatherResults: this.state.weatherResults,
+        longLatInput: this.state.longLatInput,
+        zipCodeInput: ""
+      });
+      return;
+    }
+
+    
+      this.setState({
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        data: api_result,
+        input: this.state.input,
+        weatherResults: this.state.weatherResults,
+        longLatInput: this.state.longLatInput,
+        zipCodeInput: ""
+      });
+    let name = (this.state.data) ? this.state.data.name : "";
+    let visibility = (this.state.data) ? this.state.data.visibility : "";
+    let temperature = (this.state.data.main) ? this.state.data.main.temp : "";
+    let description = (this.state.data.weather) ? this.state.data.weather[0]['description'] : "";
+    let results = (this.state.weatherResults) ? this.state.weatherResults : "";
+    let imageLink = (this.state.data.weather) ? this.state.data.weather[0]['icon'] : "";
+
+    if (this.state.data.weather) {
+      imageLink = "https://openweathermap.org/img/wn/" + imageLink + "@2x.png";
+    }
+
+    if (this.state.data.main) {
+      temperature -= 273.15;
+      temperature *= 1.8000;
+      temperature += 32;
+      temperature = parseFloat(temperature).toFixed(2);
+    }
+
+    let weatherResult = <WeatherResult city={name} country={country} temperature={temperature} description={description} imageLink={imageLink}/>
+    let newWeatherResults = [...this.state.weatherResults, weatherResult];
+
+    this.setState({
+      latitude: this.state.latitude,
+      longitude: this.state.longitude,
+      data: api_result,
+      input: "",
+      weatherResults: newWeatherResults
+    });
+  }
 
   async doCity(event) {
     let values = document.getElementById("cityName").value.split(",");
@@ -282,10 +386,29 @@ class Weather extends React.Component {
 
   render() {
     let name = (this.state.data) ? this.state.data.name : "";
-    let visibility = (this.state.data) ? this.state.data.visibility : "";
+    let visibility = (this.state.data) ? "visibility: " + this.state.data.visibility + " meters" : "";
     let temperature = (this.state.data.main) ? this.state.data.main.temp : "";
+    let min_temp = (this.state.data.main) ? this.state.data.main.temp_min : "";
+    let max_temp = (this.state.data.main) ? this.state.data.main.temp_max : "";
     let description = (this.state.data.weather) ? this.state.data.weather[0]['description'] : "";
     let results = (this.state.weatherResults) ? this.state.weatherResults : "";
+    if (this.state.data.main && temperature && min_temp && max_temp) {
+
+      temperature -= 273.15;
+      temperature *= 1.8000;
+      temperature += 32;
+      temperature = parseFloat(temperature).toFixed(2);
+
+      min_temp -= 273.15;
+      min_temp *= 1.8000;
+      min_temp += 32;
+      min_temp = parseFloat(min_temp).toFixed(2);
+
+      max_temp -= 273.15;
+      max_temp *= 1.8000;
+      max_temp += 32;
+      max_temp = parseFloat(max_temp).toFixed(2);
+    }
     return (
       <div id="container-fluid">
         <h1>Weather API</h1>
@@ -296,11 +419,17 @@ class Weather extends React.Component {
         <div id="row">
           <input type="text" id="longitudeLatitude" placeholder="Find weather from longitude, latitude" value={this.state.longLatInput} onChange={this.handleChange2} />
           <button id="longLatBtn" onClick={this.doLongLat}>Search for City</button>
+        </div>
+        <div id="row">
+          <input type="text" id="zipCode" placeholder="Find weather from zipcode, country" value={this.state.zipCodeInput} onChange={this.handleChange3} />
+          <button id="zipCodeBtn" onClick={this.doZip}>Search for City</button>
+        </div>
           <h3>{name}</h3>
           <h3>{visibility}</h3>
-          <h3>{temperature}</h3>
+          <h3>temperature: {temperature}<sup>o</sup>F</h3>
+          <h3>min: {min_temp}<sup>o</sup>F, max: {max_temp}<sup>o</sup>F</h3>
           <h3>{description}</h3>
-        </div>
+        
         <div id="results">
           {results}
         </div>
